20423ad981fd94567da5fd944f7ad8eb9d10c946
Marois2000 <tm1207@plymouth.edu>
2023-11-27 00:56:32 -0500

    Changes to winter months

--- app/Program.cs
+++ app/Program.cs
@@ -1,91 +1,115 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Windows.Markup;
 
-
-
-namespace unity
+class Program
 {
-    class Program
+    public Program() { }
+
+    // Parameters (Data is fit for 1994-2016)
+    double mooseR = 0.001;
+    double tickR = 0.5;
+    double mooseA = 0.000027;
+    double tickA = 15;
+    double mooseK = 10000;
+    double tickK = 9349440000;
+
+    // Define the system of ODEs as a function
+    List<double> SystemOfODEs(List<double> y, int month, bool longWinter)
     {
+        double dy1dt, dy2dt;
 
-        public Program() {
-
-        }
-
-        //Parameters
-        static float mooseR = 0.001f;
-        static float tickR = 0.5f;
-        static float mooseA = 0.000027f;
-        static float tickA = 15f;
-        static int mooseK = 10000;
-        static long tickK = 9349440000;
-
-        //Time Stuff
-        static int years = 100;
-        static float t0 = 0f;
-        static float h = 0.01f; //Step size
-        static float tFinal = 3.65f * years;
-        static int dayCount = 0;
-        static int monthCount = 0;
-
-        static float[] y0 = {3200f, 275000000f};
-
-        static List<float> t_values = new List<float>();
-        static List<float[]> y_values = new List<float[]>();
-
-        static float t = t0;
-        static float[] y = y0;
-
-        public float[] systemOfODEs(float t, float[] y, int month, bool longWinter) {
-            float dy1dt = 0f;
-            float dy2dt = 0f;
-
-            if(month % 12 == 11 || month % 12 == 12) {
-                dy1dt = y[0] * (1 - (y[0] + mooseA * y[1]) / mooseK);
-            } else {
-                dy1dt = mooseR * y[0] * (1 - (y[0] + mooseA * y[1]) / mooseK);
-            }
-
-            if(longWinter) {
-                if(month % 12 == 10 || month % 12 == 11 || month % 12 == 12 || month % 12 == 9) {
-                    dy2dt = -tickR*3 * y[1] * (1 - (y[1] + tickA * y[0]) / tickK);
-                } else {
-                    dy2dt = tickR * y[1] * (1 - (y[1] + tickA * y[0]) / tickK);
-                }
-            } else {
-                if(month % 12 == 10 || month % 12 == 11 || month % 12 == 12 || month % 12 == 9) {
-                    dy2dt = -tickR*4.93f * y[1] * (1 - (y[1] + tickA * y[0]) / tickK);
-                } else {
-                    dy2dt = tickR * y[1] * (1 - (y[1] + tickA * y[0]) / tickK);
-                }
-            }
-
-            return [dy1dt, dy2dt];
-        }
-
-        static void Main(string[] args) 
+        if (month % 12 == 11 || month % 12 == 0)
         {
-            Program pro = new Program();
-            t_values.Add(t0);
-            y_values.Add(y0);
+            dy1dt = y[0] * (1 - (y[0] + mooseA * y[1]) / mooseK); // Example ODE 1
+        }
+        else
+        {
+            dy1dt = mooseR * y[0] * (1 - (y[0] + mooseA * y[1]) / mooseK); // Example ODE 1
+        }
 
-            while (t < tFinal) {
-                float[] k1 = pro.systemOfODEs(t, y, monthCount, true);
-                float[] yNext = {y[0] + h * k1[0], y[1] + h * k1[1]};
-                t += h;
-                dayCount += 1;
-                y = yNext;
-
-                if(dayCount % 30 == 0) {
-                    t_values.Add(monthCount);
-                    y_values.Add(y);
-                    monthCount += 1;
-                }
-            }
-
-            foreach (float[] value in y_values)
+        if (longWinter)
+        {
+            if (month % 12 == 10 || month % 12 == 11 || month % 12 == 0 || month % 12 == 9)
             {
-                Console.WriteLine("Moose: " + value[0] + " Tick: " + value[1]);
+                dy2dt = -tickR * 2 * y[1] * (1 - (y[1] + tickA * y[0]) / tickK);  // Example ODE 2
+            }
+            else
+            {
+                dy2dt = tickR * y[1] * (1 - (y[1] + tickA * y[0]) / tickK); // Example ODE 2
             }
         }
+        else
+        {
+            if (month % 12 == 10 || month % 12 == 11 || month % 12 == 0)
+            {
+                dy2dt = -tickR * 2.89 * y[1] * (1 - (y[1] + tickA * y[0]) / tickK);  // Example ODE 2
+            }
+            else
+            {
+                dy2dt = tickR * y[1] * (1 - (y[1] + tickA * y[0]) / tickK); // Example ODE 2
+            }
+        }
+
+        return new List<double> { dy1dt, dy2dt };
+    }
+
+    static void Main()
+    {
+        // Define the time interval and step size
+        int years = 30;
+        double t0 = 0;
+        double h = 0.01;  // Step size
+        double t_final = 3.65 * years;
+        int dayCount = 0;
+        int monthCount = 0;
+
+        // Initial conditions for the system
+        // Values are fit for 1994-2016
+        List<double> y0 = new List<double> { 3200, 275000000 };  // Initial values for y1 and y2
+
+        // Create lists to store time and solution values
+        List<double> t_values = new List<double> { t0 };
+        List<List<double>> y_values = new List<List<double>> { y0 };
+
+        // Perform the Euler method in a loop
+        double t = t0;
+        List<double> y = new List<double>(y0);
+
+        List<List<double>> vals = new List<List<double>>();
+
+        while (t < t_final)
+        {
+            List<double> k1 = new List<double>(new Program().SystemOfODEs(y, monthCount, true));
+            List<double> y_next = new List<double> { y[0] + h * k1[0], y[1] + h * k1[1] };  // Euler's method update
+            t += h;
+            dayCount++;
+
+            y = new List<double>(y_next);
+
+            // Append the current time and solution values to the lists
+            if (dayCount % 30 == 0)
+            {
+                t_values.Add(monthCount);
+                y_values.Add(new List<double>(y));
+                monthCount++;
+                List<double> turn = new List<double>();
+                turn.Add(y[0]);
+                turn.Add(y[1]);
+                turn.Add(monthCount);
+                vals.Add(turn);
+            }
+        }
+
+        foreach (List<double> values in vals)
+        {
+            Console.WriteLine("Month: " + values[2]  +" Moose: " + Math.Floor(values[0]) + " Tick: " + Math.Floor(values[1]));
+        }
+
+        
     }
 }
+
+
+
--- eulersolver.py
+++ eulersolver.py
@@ -4,45 +4,50 @@ import matplotlib as plt
 from matplotlib.pyplot import subplots
 from matplotlib.pyplot import show
 
-#params (Data is fit for 1994-2016)
+# params (Data is fit for 1994-2016)
 mooseR = 0.001
 tickR = 0.5
-mooseA = .000027
+mooseA = 0.000027
 tickA = 15
 mooseK = 10000
 tickK = 9349440000
 
+
 # Define the system of ODEs as a list of functions [dy1/dt, dy2/dt, ...]
 def system_of_odes(t, y, month, longWinter):
-    if month % 12 == 11 or month % 12 == 12:
-        dy1dt =  y[0] * (1 - (y[0] + mooseA * y[1]) / mooseK) # Example ODE 1
+    if month % 12 == 11 or month % 12 == 0:
+        dy1dt = y[0] * (1 - (y[0] + mooseA * y[1]) / mooseK)  # Example ODE 1
     else:
-        dy1dt = mooseR * y[0] * (1 - (y[0] + mooseA * y[1]) / mooseK) # Example ODE 1
+        dy1dt = mooseR * y[0] * (1 - (y[0] + mooseA * y[1]) / mooseK)  # Example ODE 1
 
     if longWinter:
-        if month % 12 == 10 or month % 12 == 11 or month % 12 == 12 or month % 12 == 9:
-            dy2dt = -tickR*3 * y[1] * (1 - (y[1] + tickA * y[0]) / tickK)  # Example ODE 2
+        if month % 12 == 10 or month % 12 == 11 or month % 12 == 0 or month % 12 == 9:
+            dy2dt = (
+                -tickR * 2 * y[1] * (1 - (y[1] + tickA * y[0]) / tickK)
+            )  # Example ODE 2
         else:
-            dy2dt = tickR * y[1] * (1 - (y[1] + tickA * y[0]) / tickK) # Example ODE 2
+            dy2dt = tickR * y[1] * (1 - (y[1] + tickA * y[0]) / tickK)  # Example ODE 2
     else:
-        if month % 12 == 10 or month % 12 == 11 or month % 12 == 12:
-            dy2dt = -tickR*4.93 * y[1] * (1 - (y[1] + tickA * y[0]) / tickK)  # Example ODE 2
+        if month % 12 == 10 or month % 12 == 11 or month % 12 == 0:
+            dy2dt = (
+                -tickR * 2.89 * y[1] * (1 - (y[1] + tickA * y[0]) / tickK)
+            )  # Example ODE 2
         else:
-            dy2dt = tickR * y[1] * (1 - (y[1] + tickA * y[0]) / tickK) # Example ODE 2
-
+            dy2dt = tickR * y[1] * (1 - (y[1] + tickA * y[0]) / tickK)  # Example ODE 2
 
     return [dy1dt, dy2dt]
 
+
 # Define the time interval and step size
-years = 100
+years = 10
 t0 = 0
-h = .01  # Step size
+h = 0.01  # Step size
 t_final = 3.65 * years
 dayCount = 0
 monthCount = 0
 
 # Initial conditions for the system
-#Values are fit for 1994-2016
+# Values are fit for 1994-2016
 y0 = [3200, 275000000]  # Initial values for y1 and y2
 
 # Create arrays to store time and solution values
@@ -65,17 +70,17 @@ while t < t_final:
         y_values.append(y.tolist())
         monthCount += 1
 
-#Print the results
+# Print the results
 for t, (y1, y2) in zip(t_values, y_values):
-    print(f"t = {t}, y1 = {np.round(y1).astype(int)}, y2 = {y2}")
+    print(f"Moose = {np.round(y1).astype(int)}, Ticks = {y2}")
 
 y_values = np.array(y_values)
 y1_values = y_values[:, 0]
 y2_values = y_values[:, 1]
 
-f,(ax1, ax2) = subplots(2)
-line1, = ax1.plot(t_values, y1_values, color="b")
-line2, = ax2.plot(t_values, y2_values, color="r")
+f, (ax1, ax2) = subplots(2)
+(line1,) = ax1.plot(t_values, y1_values, color="b")
+(line2,) = ax2.plot(t_values, y2_values, color="r")
 
 ax1.set_title("The Next 100 years\nLong Winters")
 ax1.set_ylabel("Moose Population")
