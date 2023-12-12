8c76f350faa32da2bf29883ff5175d4535de4e84
Marois2000 <tm1207@plymouth.edu>
2023-12-12 01:16:54 -0500

    downloads

--- client/src/sections/simulation.jsx
+++ client/src/sections/simulation.jsx
@@ -21,11 +21,23 @@ export const Simulation = () => {
 
                         <ParagraphHeader text="Mac:"/>
                         <ol className="w-[50ch] text-left list-decimal">
-                            <li className="text-black font-montserrat">Navigate to your downloads location</li>
+                            <li className="text-black font-montserrat">Navigate to your downloads location and double click the download to unzip it</li>
+                            <li className="text-black font-montserrat">Open up Terminal and type in <br /> <span className="bg-neutral-100 p-1">chmod -R a+xr "Path/To/Your/MacBuild.app"</span> <br />The path should be Downloads/MacBuild.</li>
+                            <li className="text-black font-montserrat">Double Click on MacBuild now, it shouldn't run for security reasons.</li>
+                            <li className="text-black font-montserrat">Go to settings and then Privacy and Security</li>
+                            <li className="text-black font-montserrat">Scroll down to secuirty it should say "MacBuild" was blocked</li>
+                            <li className="text-black font-montserrat">Press "open anyway"</li>
+                            
+                            
+
+
+
+
+
                         </ol>
                     </div>
 
-                    <div className="flex justify-center items-center gap-5">
+                    <div className="flex justify-center items-center gap-5 flex-col">
                         <button className="bg-green-200 border-2 border-green-300 text-lg text-white rounded-md px-2 py-1 hover:bg-green-100 z-10 duration-300"> 
                             <a href="https://drive.google.com/uc?export=download&id=1cJg-uYGeSUdTB68qm0PkQP0wGxKB9Qov" download="Simulation.zip">Windows Download</a>
                         </button>
