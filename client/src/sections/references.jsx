67e09d7c7b8b786e59ba447ee100b023c1ad59f8
Marois2000 <tm1207@plymouth.edu>
2023-12-13 00:22:22 -0500

    references

--- client/src/sections/references.jsx
+++ client/src/sections/references.jsx
@@ -7,9 +7,13 @@ export const References = () => {
     return (
         <div className="w-full flex flex-col justify-center items-center bg-neutral-200 pb-10" id="references">
             <h2 className="text-5xl text-brown-300 font-montserrat font-semibold text-center mt-10">References</h2>
-            <ol className="w-[100ch] text-left mt-10 list-decimal max-md:w-full max-md:px-20">
-                <li className="text-black font-montserrat mt-3">“CK12-Foundation.” CK12, <a href="flexbooks.ck12.org/cbook/ck-12-biology-flexbook-2.0/r135/section/6.14/primary/lesson/competition-bio/">flexbooks.ck12.org/cbook/ck-12-biology-flexbook-2.0/r135/section/6.14/primary/lesson/competition-bio/</a> . Accessed 1 Dec. 2023. </li>
-                <li className="text-black font-montserrat mt-3">Kusnetz, Nicholas. “Climate Change Is Killing New England’s Moose. Can Hunters Save Them?” Inside Climate News, 5 Dec. 2020, insideclimatenews.org/news/29052017/climate-change-ticks-killing-new-england-moose-hunters/. </li>
+            <ol className="w-[100ch] text-left mt-10 list-decimal">
+                <li className="text-black font-montserrat mt-3">“CK12-Foundation.” CK12, <u><a href="http://ck12.org/cbook/ck-12-biology-flexbook-2.0/r135/section/6.14/primary/lesson/competition-bio/" target="_blank">flexbooks.ck12.org/cbook/ck-12-biology-flexbook-2.0/r135/section/6.14/primary/lesson/competition-bio/ </a></u>. Accessed 1 Dec. 2023. </li>
+                <li className="text-black font-montserrat mt-3">Kusnetz, Nicholas. “Climate Change Is Killing New England’s Moose. Can Hunters Save Them?” Inside Climate News, 5 Dec. 2020, <u><a href="http://insideclimatenews.org/news/29052017/climate-change-ticks-killing-new-england-moose-hunters/" target="_blank">insideclimatenews.org/news/29052017/climate-change-ticks-killing-new-england-moose-hunters/</a></u>. </li>
+                <li className="text-black font-montserrat mt-3">“Moose.” State of New Hampshire Fish and Game, <u><a href="http://www.wildlife.nh.gov/wildlife-and-habitat/species-occurring-nh/moose#:~:text=The%20current%20New%20Hampshire%20moose" target="_blank">www.wildlife.nh.gov/wildlife-and-habitat/species-occurring-nh/moose#:~:text=The%20current%20New%20Hampshire%20moose</a></u>,1.73%20being%20in%20the%20middle. Accessed 12 Dec. 2023.  </li>
+                <li className="text-black font-montserrat mt-3">“Tracking New Hampshire’s Moose Population Using Drones and Wildlife Cameras.” UNH Today, 9 Sept. 2023, <u><a href="http://www.unh.edu/unhtoday/2023/02/population-surveying-methods-new-hampshire-moose" target="_blank">www.unh.edu/unhtoday/2023/02/population-surveying-methods-new-hampshire-moose.</a></u> </li>
+                <li className="text-black font-montserrat mt-3">“Lotka-Volterra Model.” Lotka-Volterra Model - an Overview | ScienceDirect Topics, <u><a target="_blank" href="http://www.sciencedirect.com/topics/earth-and-planetary-sciences/lotka-volterra-model#:~:text=The%20Lotka%E2%80%93Volterra%20model%20explores,not%20explicitly%20stated%20or%20explored">www.sciencedirect.com/topics/earth-and-planetary-sciences/lotka-volterra-model#:~:text=The%20Lotka%E2%80%93Volterra%20model%20explores,not%20explicitly%20stated%20or%20explored</a></u>. Accessed 13 Dec. 2023. </li>
+
             </ol>
         </div>
     )
