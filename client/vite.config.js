26e8359f12576a2ef9162d98bf6f4b9ad0cb4b85
Marois2000 <tm1207@plymouth.edu>
2024-06-07 21:07:26 -0400

    moved to linux server

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Moose-Population-Simulation",
+  base: "/",
   plugins: [react()],
 })
